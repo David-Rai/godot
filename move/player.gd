0203a7f2bae545a1e7bceefc245d403e348906a6
david-rai <davidkhaling123@gmail.com>
2025-11-10 02:47:32 +0545

    Normal character move added

--- move/player.gd
+++ move/player.gd
@@ -1,22 +1,19 @@
-extends Sprite2D
+extends CharacterBody2D
 
 var speed=200
-var direction=Vector2.ZERO
 
-func _ready() -> void:
-	pass # Replace with function body.
-
+# Runs on every physics frame per rate
 func _physics_process(delta: float) -> void:
-	move_character(delta)
-	
-func move_character(delta:float):
-	direction=Vector2.ZERO
-	if Input.is_action_pressed("left"):
-		direction.x= -1
-	if Input.is_action_pressed("right"):
-		direction.x=1
-	if Input.is_action_pressed("top"):
-		direction.y= -1
-	if Input.is_action_pressed("bottom"):
-		direction.y= 1
-	position +=direction.normalized() * speed * delta	
+	#var direction=Vector2.ZERO
+	#if Input.is_action_pressed("left"):
+		#direction.x=-1
+	#if Input.is_action_pressed("right"):
+		#direction.x=1
+	#if Input.is_action_pressed("top"):
+		#direction.y=-1
+	#if Input.is_action_pressed("bottom"):
+		#direction.y=1
+	var direction = Input.get_vector('left','right','top','bottom')
+	print(direction)
+	velocity =direction.normalized() * speed
+	move_and_slide()
