00e1e9ae3279382ee317d221b914b94227712f89
david-rai <davidkhaling123@gmail.com>
2025-11-11 14:40:43 +0545

    Feat:Added better collision detection , Engine slow down , queue_free()

--- move/scripts/killzone.gd
+++ move/scripts/killzone.gd
@@ -6,7 +6,9 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("someone is dead")
+	Engine.time_scale=0.5#slowing the engine
 	countdown.start()
 
 func _on_countdown_timeout() -> void:
+	Engine.time_scale = 1 #restting the engine settings
 	get_tree().reload_current_scene()
--- move/scripts/moving_enemy.gd
+++ move/scripts/moving_enemy.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var enemy: Area2D = $enemy
+@onready var sprite=enemy.get_node('AnimatedSprite2D')
 @onready var ray_cast_left: RayCast2D = $enemy/RayCastLeft
 @onready var ray_cast_right: RayCast2D = $enemy/RayCastRight
 
@@ -9,8 +10,14 @@ var direction=1
 
 #Moving enemies
 func _process(delta: float) -> void:
+
 	if ray_cast_left.is_colliding():
+		sprite.flip_h=false
 		direction = 1
+	
 	if ray_cast_right.is_colliding():
+		sprite.flip_h=true
 		direction = -1	 
+	
 	enemy.position.x +=direction * speed * delta  
+		
